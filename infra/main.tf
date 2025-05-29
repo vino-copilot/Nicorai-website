12ab3b6e4930dac8c46b1290b7dca4cc16ead6fc
abhinand27tech <abhinand.nicorai@gmail.com>
2025-05-29 13:36:45 +0530

    bug fix

--- infra/main.tf
+++ infra/main.tf
@@ -80,8 +80,8 @@ resource "aws_cloudfront_distribution" "frontend" {
 
     viewer_protocol_policy = "redirect-to-https"
     min_ttl                = 0
-    default_ttl            = 3600
-    max_ttl                = 86400
+    default_ttl            = 0
+    max_ttl                = 0
   }
 
   restrictions {
